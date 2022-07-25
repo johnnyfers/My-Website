8e83e265edb2c758827d3d09d82bda1153bda1cd
Joao Ferreira <joaovlima07@hotmail.com>
2022-07-25 18:53:09 -0300

    feat: blog and views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tag, Author, Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date')
+    list_filter = ('author', 'date', 'tags')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Tag)
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
